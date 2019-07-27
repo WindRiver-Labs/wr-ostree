c201f80788f5ea237e250377e4361be349817088
Jason Wessel <jason.wessel@windriver.com>
2019-07-27 07:20:22 -0700

    ostree_upgrade_x86_64.sh: Add ARM support to upgrade script
    This commit adds ARM support and the next commit will rename the file
    now that the upgrade code is shared for arm and x86.
    
    [ Issue: LINUXEXEC-3159 ]
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- recipes-support/ostree-upgrade-mgr/ostree-upgrade-mgr/ostree_upgrade_x86_64.sh
+++ recipes-support/ostree-upgrade-mgr/ostree-upgrade-mgr/ostree_upgrade_x86_64.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-#  This script is supposed to run on intel-x86 target with grub-efi installed.
+#  This script will use OSTree to upgrade the partitions which are not
+#  in use and update either the grub or u-boot to start the upgraded
+#  partition on the next reboot.
 #
-#* Copyright (c) 2018 Wind River Systems, Inc.
+#* Copyright (c) 2018-2019 Wind River Systems, Inc.
 #* 
 #* This program is free software; you can redistribute it and/or modify
 #* it under the terms of the GNU General Public License version 2 as
@@ -20,6 +22,9 @@
 UPGRADE_ROOTFS_DIR=""
 UPGRADE_BOOT_DIR=""
 UPGRADE_ESP_DIR=""
+CLEANUP_MOUNTS=""
+DEBUG_SKIP_FSCK=${DEBUG_SKIP_FSCK=0}
+USE_GRUB=1
 BACKUP_PART_INDICATOR="_b"
 GRUB_EDITENV_BIN=$(which grub-editenv)
 GRUB_ENV_FILE="/boot/efi/EFI/BOOT/boot.env"
@@ -29,27 +34,17 @@ BOOT_VAR="boot_part"
 ROOT_VAR="root_part"
 MOUNT_FLAG="rw,noatime,iversion"
 
-[ -f $GRUB_EDITENV_BIN ] || {
-	echo "grub-editenv is not found on target."
-	echo "This script should run on intel-x86 platform with grub-efi installed!"
-	exit 1
-}
-
-[ -f $GRUB_ENV_FILE ] || {
-	$GRUB_EDITENV_BIN $GRUB_ENV_FILE create
-}
-
 cleanup() {
-	umount $UPGRADE_ESP_DIR 
-	umount $UPGRADE_BOOT_DIR 
-	umount $UPGRADE_ROOTFS_DIR
+	for d in $CLEANUP_MOUNTS ; do
+		umount $d
+	done
 
-	rm -rf $UPGRADE_ROOTFS_DIR	
+	rmdir $UPGRADE_ROOTFS_DIR
 }
 # get the label name for boot partition to be upgraded
 get_upgrade_part_label() {
-	local labelroot=`cat /proc/cmdline |tr " " "\n" |grep ostree_root | awk -F '=' '{print $3}'`
-	local labelboot=`cat /proc/cmdline |tr " " "\n" |grep ostree_boot | awk -F '=' '{print $3}'`
+	local labelroot=`cat /proc/cmdline |tr " " "\n" |sort -u |grep ostree_root | awk -F '=' '{print $3}'`
+	local labelboot=`cat /proc/cmdline |tr " " "\n" |sort -u |grep ostree_boot | awk -F '=' '{print $3}'`
 
 	echo "$labelroot" | fgrep "${BACKUP_PART_INDICATOR}" >> /dev/null
 	if [ $? -ne 0 ]; then
@@ -69,7 +64,7 @@ get_upgrade_part_label() {
 
 # ESP device
 get_esp_dev() {
-	mount |grep /boot/efi | cut -f 1 -d " "
+	cat /proc/mounts |grep \ /boot/efi | cut -f 1 -d " "
 }
 
 create_dir() {
@@ -90,9 +85,36 @@ prepare_mount() {
 	UPGRADE_BOOT_DIR="$UPGRADE_ROOTFS_DIR/boot"
 	UPGRADE_ESP_DIR="$UPGRADE_BOOT_DIR/efi"
 
-	mount -o $MOUNT_FLAG "LABEL=$1" "$UPGRADE_ROOTFS_DIR" || fatal "Error mounting LABEL=$1"
-	mount -o $MOUNT_FLAG "LABEL=$2" "$UPGRADE_BOOT_DIR" || fatal "Error mounting LABEL=$2"
-	mount -o $MOUNT_FLAG "$3" $UPGRADE_ESP_DIR || fatal "Error mounting $3"
+	# Detect correct disk labels from the running disk
+	sysrootdev=$(cat /proc/mounts |grep \ /sysroot\  |awk '{print $1}')
+	[ "$sysrootdev" = "" ] && fatal "Could not find mounted /sysroot"
+
+	RAWDEV=$(lsblk -npo pkname $sysrootdev)
+	[ "$sysrootdev" = "" ] && fatal "Could not find raw device"
+
+	dev=$(lsblk -rpno label,kname $RAWDEV|grep ^$1\ |awk '{print $2}')
+	[ "$dev" = "" ] && fatal "Error finding LABEL=$1"
+	mount -o $MOUNT_FLAG $dev "$UPGRADE_ROOTFS_DIR" || fatal "Error mounting LABEL=$1"
+	CLEANUP_MOUNTS="$UPGRADE_ROOTFS_DIR"
+
+
+	dev=$(lsblk -rpno label,kname $RAWDEV|grep ^$2\ |awk '{print $2}')
+	[ "$dev" = "" ] && fatal "Error finding LABEL=$2"
+	mount -o $MOUNT_FLAG $dev "$UPGRADE_BOOT_DIR" || fatal "Error mounting LABEL=$2"
+	CLEANUP_MOUNTS="$UPGRADE_BOOT_DIR $CLEANUP_MOUNTS"
+
+	if [ "$3" != "" ] ; then
+		# This volume is "special" and might already be mounted RO, and a
+		# remount requires manipulating the state.
+		mount -r "$3" $UPGRADE_ESP_DIR 2> /dev/null
+		if [ $? = 0 ] ; then
+			CLEANUP_MOUNTS="$UPGRADE_ESP_DIR $CLEANUP_MOUNTS"
+			mount -o remount,rw "$3" $UPGRADE_ESP_DIR 2> /dev/null || fatal "Error mounting $3"
+		else
+			mount -w "$3" $UPGRADE_ESP_DIR || fatal "Error mounting $3"
+			CLEANUP_MOUNTS="$UPGRADE_ESP_DIR $CLEANUP_MOUNTS"
+		fi
+	fi
 }
 
 check_repo_url() {
@@ -106,25 +128,19 @@ check_repo_url() {
 
 	if [ -z "${branch}" ] ; then
 		echo "No branch specified for upgrade, please configure it via:"
-		echo " ostree config set upgrade.branch <branch>"
-		cleanup
-		exit 1
+		fatal " ostree config set upgrade.branch <branch>"
 	fi
 
 	if [ -z "${remote}" ] ; then
 		echo "No remote repository specified for upgrade, please configure it via:"
-		echo " ostree config set upgrade.remote <remote_repo_name>"
-		cleanup
-		exit 1
+		fatal " ostree config set upgrade.remote <remote_repo_name>"
 	fi
 
 	url=`ostree remote --repo=/sysroot/ostree/repo show-url ${remote} 2> /dev/null`
 
 	if [ -z "${url}" ] ; then
 		echo "No remote repository url configured, please configure it via:"
-		echo " ostree remote add ${remote} <url>"
-		cleanup
-		exit 1
+		fatal " ostree remote add ${remote} <url>"
 	fi
 
 	# Copy the existing configuration to the upgrade partition
@@ -134,6 +150,19 @@ check_repo_url() {
 
 prepare_upgrade() {
 	UPGRADE_ESP_DEV=$(get_esp_dev)
+	if [ "${UPGRADE_ESP_DEV}" = "" ] ; then
+		USE_GRUB=0
+	else
+		[ -f "$GRUB_EDITENV_BIN" ] || {
+			echo "grub-editenv is not found on target."
+			echo "This script should run on intel-x86 platform with grub-efi installed!"
+			exit 1
+		}
+
+		[ -f "$GRUB_ENV_FILE" ] || {
+			$GRUB_EDITENV_BIN $GRUB_ENV_FILE create
+		}
+	fi
 	get_upgrade_part_label
 
 	prepare_mount $UPGRADE_ROOT_LABEL $UPGRADE_BOOT_LABEL $UPGRADE_ESP_DEV
@@ -160,21 +189,19 @@ ostree_upgrade() {
 
 	if [ "${os}" = "" ] ; then
 	    echo "Error deploy OS is not defined, please configure it via:"
-	    echo " ostree config set upgrade.os <DEPLOY_OS_NAME>"
-	    cleanup
-	    exit 1
+	    fatal " ostree config set upgrade.os <DEPLOY_OS_NAME>"
 	fi
 
 	# Perform repairs, if needed on the upgrade ostree repository
 	repair=0
-	ostree fsck -a --delete --repo=$UPGRADE_ROOTFS_DIR/ostree/repo
+	[ $DEBUG_SKIP_FSCK = 1 ] || ostree fsck -a --delete --repo=$UPGRADE_ROOTFS_DIR/ostree/repo
 	if [ $? != 0 ] ; then
 		repair=1
 	fi
 
 	lcache="--localcache-repo=/sysroot/ostree/repo"
 
-	ostree fsck --repo=/sysroot/ostree/repo
+	[ $DEBUG_SKIP_FSCK = 1 ] || ostree fsck --repo=/sysroot/ostree/repo
 	if [ $? = 0 ] ; then
 		ostree pull $lcache --repo=$UPGRADE_ROOTFS_DIR/ostree/repo ${remote}:${branch}
 		# Always try a cached pull first so as not to incur extra bandwidth cost
@@ -189,9 +216,7 @@ ostree_upgrade() {
 		ostree pull --repo=$UPGRADE_ROOTFS_DIR/ostree/repo ${remote}:${branch}
 	fi
 	if [ $? -ne 0 ]; then
-		echo "Ostree pull failed"
-		cleanup
-		exit 1
+		fatal "Ostree pull failed"
 	fi
 	if [ $repair = 1 ] ; then
 		# Repair any other remote references if the original was damaged and re-run fsck
@@ -199,25 +224,19 @@ ostree_upgrade() {
 			if [ "${remote}:${branch}" != "$b" ] ; then
 				ostree pull $lcache --repo=$UPGRADE_ROOTFS_DIR/ostree/repo $b
 				if [ $? -ne 0 ]; then
-					echo "Ostree pull failed"
-					cleanup
-					exit 1
+					fatal "Ostree pull failed"
 				fi
 			fi
 		done
 		ostree fsck -a --delete --repo=$UPGRADE_ROOTFS_DIR/ostree/repo
 		if [ $? != 0 ] ; then
-			echo "Error: Upgrade partition ostree repo could not be repaired"
-			cleanup
-			exit 1
+			fatal "Error: Upgrade partition ostree repo could not be repaired"
 		fi
 	fi
 
 	ostree admin --sysroot=$UPGRADE_ROOTFS_DIR deploy --os=${os} ${branch}
 	if [ $? -ne 0 ]; then
-		echo "Ostree deploy failed"
-		cleanup
-		exit 1
+		fatal "Ostree deploy failed"
 	fi
 	check=`ostree config get upgrade.noflux 2>/dev/null`
 	if [ "$check" = 1 ] ; then
@@ -228,9 +247,24 @@ ostree_upgrade() {
 }
 
 update_env() {
-	$GRUB_EDITENV_BIN $GRUB_ENV_FILE set \
-		rollback_part=$ROLLBACK_VAL $BOOTMODE_VAR=$BOOTMODE_VAL \
-		default=0 boot_tried_count=0
+	if [ $USE_GRUB = 1 ] ; then
+		$GRUB_EDITENV_BIN $UPGRADE_ROOTFS_DIR/$GRUB_ENV_FILE set \
+			rollback_part=$ROLLBACK_VAL $BOOTMODE_VAR=$BOOTMODE_VAL \
+			default=0 boot_tried_count=0
+	else
+		# Assume this is a u-boot volume to update
+		tmpdir=$(mktemp -d /tmp/boot.XXXXX)
+		dev=$(lsblk -rpno label,kname $RAWDEV|grep ^boot\ |awk '{print $2}')
+		[ "$dev" = "" ] && fatal "Error finding LABEL=boot"
+		mount -w $dev $tmpdir || fatal "Error mounting LABEL=boot"
+		CLEANUP_MOUNTS="$tmpdir $CLEANUP_MOUNTS"
+		abflag="B"
+		if [ "$BOOTMODE_VAL" = "" ] ; then
+			abflag="A"
+		fi
+		printf "123"${abflag} > $tmpdir/boot_ab_flag
+		printf '0\0WR' > $tmpdir/boot_cnt
+	fi
 }
 
 run_upgrade() {
