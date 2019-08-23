59b15a056ae77ef714ca89facd4047ab98a1bb6f
Jason Wessel <jason.wessel@windriver.com>
2019-08-23 12:59:52 -0700

    init-ostree-install.sh: Fix white space
    Tabs were invented for a reason:
    
    ls -l Before:
    -rw-r--r-- 1 jwessel users 7955 Aug 23 13:01 recipes-sota/initrdscripts/files/init-ostree.sh
    
    ls -l After:
    -rw-r--r-- 1 jwessel users 7578 Aug 23 13:01 recipes-sota/initrdscripts/files/init-ostree.sh
    
    The formatting needed to be fixed before adding more code.
    
    [ Issue: LINUXEXEC-3159 ]
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- recipes-sota/initrdscripts/files/init-ostree.sh
+++ recipes-sota/initrdscripts/files/init-ostree.sh
@@ -63,82 +63,82 @@ do_mount_fs() {
 
 early_setup() {
 
-    do_mount_fs proc /proc
-    do_mount_fs sysfs /sys
-    mount -t devtmpfs none /dev
-    do_mount_fs tmpfs /tmp
-    do_mount_fs tmpfs /run
+	do_mount_fs proc /proc
+	do_mount_fs sysfs /sys
+	mount -t devtmpfs none /dev
+	do_mount_fs tmpfs /tmp
+	do_mount_fs tmpfs /run
 
-    $_UDEV_DAEMON --daemon
-    udevadm trigger --action=add
+	$_UDEV_DAEMON --daemon
+	udevadm trigger --action=add
 
-    if [ -x /sbin/mdadm ]; then
-	/sbin/mdadm -v --assemble --scan --auto=md
-    fi
+	if [ -x /sbin/mdadm ]; then
+		/sbin/mdadm -v --assemble --scan --auto=md
+	fi
 }
 
 read_args() {
-    [ -z "$CMDLINE" ] && CMDLINE=`cat /proc/cmdline`
-    for arg in $CMDLINE; do
-        optarg=`expr "x$arg" : 'x[^=]*=\(.*\)'`
-        case $arg in
-            ostree_root=*)
-		OSTREE_ROOT_DEVICE=$optarg ;;
-            root=*)
-                ROOT_DEVICE=$optarg ;;
-            rootdelay=*)
-                ROOT_DELAY=$optarg ;;
-            skip-boot-diff=*)
-		SKIP_BOOT_DIFF=$optarg ;;
-            rootflags=*)
-		ROOT_FLAGS=$optarg ;;
-            init=*)
-                INIT=$optarg ;;
-            ostree_boot=*)
-		OSTREE_BOOT_DEVICE=$optarg ;;
-            flux=*)
-                OSTREE_LABEL_FLUXDATA=$optarg ;;
-        esac
-    done
+	[ -z "$CMDLINE" ] && CMDLINE=`cat /proc/cmdline`
+	for arg in $CMDLINE; do
+		optarg=`expr "x$arg" : 'x[^=]*=\(.*\)'`
+		case $arg in
+			ostree_root=*)
+				OSTREE_ROOT_DEVICE=$optarg ;;
+			root=*)
+				ROOT_DEVICE=$optarg ;;
+			rootdelay=*)
+				ROOT_DELAY=$optarg ;;
+			skip-boot-diff=*)
+				SKIP_BOOT_DIFF=$optarg ;;
+			rootflags=*)
+				ROOT_FLAGS=$optarg ;;
+			init=*)
+				INIT=$optarg ;;
+			ostree_boot=*)
+				OSTREE_BOOT_DEVICE=$optarg ;;
+			flux=*)
+				OSTREE_LABEL_FLUXDATA=$optarg ;;
+		esac
+	done
 }
 
 expand_fluxdata() {
 
-   fluxdata_label=$OSTREE_LABEL_FLUXDATA
-   [ -z $fluxdata_label ] && echo "No fluxdata partition found." && return 0
+	fluxdata_label=$OSTREE_LABEL_FLUXDATA
+	[ -z $fluxdata_label ] && echo "No fluxdata partition found." && return 0
 
-   # expanding FLUXDATA
-   datapart=$(blkid -s LABEL | grep "LABEL=\"$fluxdata_label\"" |head -n 1| awk -F: '{print $1}')
+	# expanding FLUXDATA
+	datapart=$(blkid -s LABEL | grep "LABEL=\"$fluxdata_label\"" |head -n 1| awk -F: '{print $1}')
 
-   # no fluxdata or fluxdata is a LUKS(expanding done at LUKS creation)
-   [ -z ${datapart} ] && {
-	datapart=$(blkid -s LABEL | grep "LABEL=\"luks$fluxdata_label\"" |head -n 1| awk -F: '{print $1}')
-	[ -z ${datapart} ] && return 0
-   }
+	# no fluxdata or fluxdata is a LUKS(expanding done at LUKS creation)
+	[ -z ${datapart} ] && {
+		datapart=$(blkid -s LABEL | grep "LABEL=\"luks$fluxdata_label\"" |head -n 1| awk -F: '{print $1}')
+		[ -z ${datapart} ] && return 0
+	}
 
-   datadev=$(lsblk $datapart -n -o PKNAME | head -n 1)
-   datadevnum=$(echo ${datapart} | sed 's/\(.*\)\(.\)$/\2/')
+	datadev=$(lsblk $datapart -n -o PKNAME | head -n 1)
+	datadevnum=$(echo ${datapart} | sed 's/\(.*\)\(.\)$/\2/')
 
-   disk_sect=`fdisk -l /dev/$datadev | head -1 |awk '{print $7}'`
-   part_end=`fdisk -l /dev/$datadev | grep ^${datapart} | awk '{print $3}'`
-   disk_end=$(expr $disk_sect - 1026)
-   if [ $part_end -ge $disk_end ]; then
-       echo "No fluxdata expansion." && return 0
-   fi
+	disk_sect=`fdisk -l /dev/$datadev | head -1 |awk '{print $7}'`
+	part_end=`fdisk -l /dev/$datadev | grep ^${datapart} | awk '{print $3}'`
+	disk_end=$(expr $disk_sect - 1026)
+	if [ $part_end -ge $disk_end ]; then
+		echo "No fluxdata expansion." && return 0
+	fi
 
-   echo "Expanding partition for ${fluxdata_label} ..."
-   parted -s /dev/$datadev -- resizepart $datadevnum 100%
-			     
-   echo "Expanding FS for ${fluxdata_label} ..."
-   resize2fs -f ${datapart}
+	echo "Expanding partition for ${fluxdata_label} ..."
+	parted -s /dev/$datadev -- resizepart $datadevnum 100%
+
+	echo "Expanding FS for ${fluxdata_label} ..."
+	resize2fs -f ${datapart}
 }
 
 fatal() {
-    echo $1 >$CONSOLE
-    echo >$CONSOLE
-    sleep 5
-    echo b > /proc/sysrq-trigger
-    exec sh
+	echo $1 >$CONSOLE
+	echo >$CONSOLE
+	sleep 5
+	echo b > /proc/sysrq-trigger
+	exec sh
 }
 
 #######################################
@@ -150,7 +150,6 @@ read_args
 [ -z "$CONSOLE" ] && CONSOLE="/dev/console"
 [ -z "$INIT" ] && INIT="/sbin/init"
 
-
 udevadm settle --timeout=3
 
 mkdir -p $ROOT_MOUNT/
@@ -158,45 +157,45 @@ mkdir -p $ROOT_MOUNT/
 sleep ${ROOT_DELAY}
 
 [ -z $OSTREE_ROOT_DEVICE ] && fatal "No OSTREE root device specified, please add 'ostree_root=LABEL=xyz' in bootline!" || {
-    echo "Waiting for low speed devices to be ready ..."
-    OSTREE_LABEL_ROOT=$(echo $OSTREE_ROOT_DEVICE | cut -f 2 -d'=')
-    retry=0
-    # For LUKS, we might wait for MAX_TIMEOUT_FOR_WAITING_LOWSPEED_DEVICE/10s
-    while [ $retry -lt $MAX_TIMEOUT_FOR_WAITING_LOWSPEED_DEVICE ] ; do
-	retry=$(($retry+1))
-        blkid -t LABEL=$OSTREE_LABEL_ROOT && break
-        blkid -t LABEL=luks$OSTREE_LABEL_ROOT && break
-	#echo "sleep to wait for $OSTREE_ROOT_DEVICE"
-        sleep 0.1
-    done
+	echo "Waiting for low speed devices to be ready ..."
+	OSTREE_LABEL_ROOT=$(echo $OSTREE_ROOT_DEVICE | cut -f 2 -d'=')
+	retry=0
+	# For LUKS, we might wait for MAX_TIMEOUT_FOR_WAITING_LOWSPEED_DEVICE/10s
+	while [ $retry -lt $MAX_TIMEOUT_FOR_WAITING_LOWSPEED_DEVICE ] ; do
+		retry=$(($retry+1))
+		blkid -t LABEL=$OSTREE_LABEL_ROOT && break
+		blkid -t LABEL=luks$OSTREE_LABEL_ROOT && break
+		#echo "sleep to wait for $OSTREE_ROOT_DEVICE"
+		sleep 0.1
+	done
 }
 try_to_mount_rootfs() {
-    local mount_flags="rw,noatime,iversion"
-    mount_flags="${mount_flags},${ROOT_FLAGS}"
-    
-    mount -o $mount_flags "${OSTREE_ROOT_DEVICE}" "${ROOT_MOUNT}" 2>/dev/null && return 0
+	local mount_flags="rw,noatime,iversion"
+	mount_flags="${mount_flags},${ROOT_FLAGS}"
+
+	mount -o $mount_flags "${OSTREE_ROOT_DEVICE}" "${ROOT_MOUNT}" 2>/dev/null && return 0
 }
 
 expand_fluxdata
 
 [ -x /init.luks-ostree ] && {
-    /init.luks-ostree $OSTREE_LABEL_ROOT $OSTREE_LABEL_FLUXDATA && echo "LUKS init done." || fatal "Couldn't init LUKS, dropping to shell"
+	/init.luks-ostree $OSTREE_LABEL_ROOT $OSTREE_LABEL_FLUXDATA && echo "LUKS init done." || fatal "Couldn't init LUKS, dropping to shell"
 }
 
 echo "Waiting for root device to be ready..."
 while [ 1 ] ; do
-    try_to_mount_rootfs && break
-    sleep 0.1
+	try_to_mount_rootfs && break
+	sleep 0.1
 done
 
 if [ ! -d "${ROOT_MOUNT}/boot" ] ; then
-   mkdir -p ${ROOT_MOUNT}/boot
+	mkdir -p ${ROOT_MOUNT}/boot
 fi
 
 echo "Waiting for boot device to be ready..."
 while [ 1 ] ; do
-    mount "${OSTREE_BOOT_DEVICE}" "${ROOT_MOUNT}/boot" && break
-    sleep 0.1
+	mount "${OSTREE_BOOT_DEVICE}" "${ROOT_MOUNT}/boot" && break
+	sleep 0.1
 done
 
 OSTREE_DEPLOY=`ostree-prepare-root ${ROOT_MOUNT} | awk -F ':' '{print $2}'`
@@ -214,14 +213,14 @@ udevadm control -e
 
 cd $ROOT_MOUNT
 for x in dev proc sys; do
-    log_info "Moving /$x to new rootfs"
-    mount --move "/$x" "$x"
+	log_info "Moving /$x to new rootfs"
+	mount --move "/$x" "$x"
 done
 
 # If we pass args to bash, it will assume they are text files
 # to source and run.
 if [ "$INIT" == "/bin/bash" ] || [ "$INIT" == "/bin/sh" ]; then
-    CMDLINE=""
+	CMDLINE=""
 fi
 
 # Start checking ostree contents
@@ -229,37 +228,37 @@ mount -t proc none /proc
 
 # Check for skip-boot-diff
 if [ "${SKIP_BOOT_DIFF}" = 1 ] ; then
-    skip=1
+	skip=1
 else
-    skip=`ostree config --repo=/sysroot/ostree/repo get upgrade.skip-boot-diff 2> /dev/null`
+	skip=`ostree config --repo=/sysroot/ostree/repo get upgrade.skip-boot-diff 2> /dev/null`
 fi
 
 
 if [ "${skip}" != "1" ] ; then
 
-    /usr/bin/ostree fsck --repo=/sysroot/ostree/repo
-    if [ $? -ne 0 ]; then
-	    echo "Ostree repo is damaged..."
-	    fatal
-    fi
+	/usr/bin/ostree fsck --repo=/sysroot/ostree/repo
+	if [ $? -ne 0 ]; then
+		echo "Ostree repo is damaged..."
+		fatal
+	fi
 
-    ostree_ref="${OSTREE_DEPLOY##*/}"
-    ostree_ref="${ostree_ref%%.*}"
+	ostree_ref="${OSTREE_DEPLOY##*/}"
+	ostree_ref="${ostree_ref%%.*}"
 
-    if [ -z "${ostree_ref}" ]; then
-	    echo "No ostree ref found"
-	    #fatal
-	    exec sh
-    fi
+	if [ -z "${ostree_ref}" ]; then
+		echo "No ostree ref found"
+		#fatal
+		exec sh
+	fi
 
-    log_info "Checking ostree ${ostree_ref} contents... with ${OSTREE_DEPLOY}"
+	log_info "Checking ostree ${ostree_ref} contents... with ${OSTREE_DEPLOY}"
 
-    /usr/bin/ostree diff --repo=/sysroot/ostree/repo ${ostree_ref} ${OSTREE_DEPLOY}| grep "[MD] */usr/"
+	/usr/bin/ostree diff --repo=/sysroot/ostree/repo ${ostree_ref} ${OSTREE_DEPLOY}| grep "[MD] */usr/"
 
-    if [ $? -eq 0 ]; then
-	    echo "Ostree deploy content is corrupted..."
-	    fatal
-    fi
+	if [ $? -eq 0 ]; then
+		echo "Ostree deploy content is corrupted..."
+		fatal
+	fi
 fi
 
 umount /proc
@@ -270,12 +269,11 @@ umount /proc
 # signature stored in xattr. However, ramfs doesn't support xattr, and all
 # other initializations must *NOT* be placed after IMA initialization!
 [ -x /init.ima ] && /init.ima $ROOT_MOUNT && {
-    # switch_root is an exception. We call it in the real rootfs and it
-    # should be already signed properly.
-    switch_root="usr/sbin/switch_root.static"
+	# switch_root is an exception. We call it in the real rootfs and it
+	# should be already signed properly.
+	switch_root="usr/sbin/switch_root.static"
 } || {
-    switch_root="switch_root"
+	switch_root="switch_root"
 }
 
-exec $switch_root $ROOT_MOUNT $INIT $CMDLINE ||
-    fatal "Couldn't switch_root, dropping to shell"
+exec $switch_root $ROOT_MOUNT $INIT $CMDLINE || fatal "Couldn't switch_root, dropping to shell"
