WindRiver-Labs/wr-ostree

commit bb868b27706ca92621354a94fc0cb9f70ff945c3

boot.scr, u-boot-uenv: Fix network install to use $use_fdtdtb
The computation of the bootargs when using an network install or local
install from the fat partition was not correctly using the variable
$use_fdtdtb.  When the use_fdtdtb is set the boot argument must be
prepended which are found in the fdt data, else the BSP could be
unstable when arguments such as cma=256M are missing.

[ Issue: LINCD-703 ]

Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

recipes-bsp/u-boot/u-boot-uenv_2.0.bb
@@ -137,13 +137,22 @@ if test -n \${oBRANCH}; then
 else
  setenv BRANCH ${OSTREE_NETINST_BRANCH}
 fi
+setenv fdtargs
+setenv netinstpre
+if test -n \${use_fdtdtb} || test \${use_fdtdtb} -ge 1; then
+ fdt addr \${fdt_addr}
+ if test \${use_fdtdtb} -ge 2; then
+  fdt get value fdtargs /chosen bootargs
+ fi
+else
+ setenv netinstpre "fatload mmc \${mmcdev}:1 \${fdt_addr} \${fdt_file};"
+fi
 setenv exinargs
-setenv netinstpre "fdt addr \${fdt_addr};"
 setenv instdef "$NETINST_ARGS"
-if test -n \${nistargs}; then
+if test -n \${ninstargs}; then
  setenv netinst "\${ninstargs}"
 else
- setenv netinst "\${netinstpre}fatload mmc \${mmcdev}:1 \${loadaddr} Image;fatload mmc \${mmcdev}:1 \${initrd_addr} initramfs; setenv bootargs \\"\${instdef} \${exinargs}\\";${OSTREE_UBOOT_CMD} \${loadaddr} \${initrd_addr} \${fdt_addr}"
+ setenv netinst "\${netinstpre}fatload mmc \${mmcdev}:1 \${loadaddr} Image;fatload mmc \${mmcdev}:1 \${initrd_addr} initramfs; setenv bootargs \\"\${fdtargs} \${instdef} \${exinargs}\\";${OSTREE_UBOOT_CMD} \${loadaddr} \${initrd_addr} \${fdt_addr}"
 fi
 setenv autoinst echo "!!!Autostarting ERASE and INSTALL, you have 5 seconds to reset the board!!!"\;sleep 5\;run netinst
 if test "\${no_autonetinst}" != 1 && test -n \${URL} ; then
@@ -194,15 +203,8 @@ fi
 if test \${skip_script_wd} != yes; then setenv wdttimeout 120000; fi
 setenv loadkernel ext4load mmc \${mmcdev}:\${mmcpart} \${loadaddr} \${ostver}/vmlinuz
 setenv loadramdisk ext4load mmc \${mmcdev}:\${mmcpart} \${initrd_addr} \${ostver}/initramfs
-setenv bootargs \${bootpart} ostree=/ostree/\${ostver} \${rootpart} console=\${console},\${baudrate} \${smp} flux=fluxdata\${labelpre}
-if test -n \${use_fdtdtb} && test \${use_fdtdtb} -ge 1; then
- fdt addr \${fdt_addr}
- if test \${use_fdtdtb} -ge 2; then
-  setenv bootargs
-  fdt get value bootargs /chosen bootargs
-  setenv bootargs "\${bootargs} \${bootpart} ostree=/ostree/\${ostver} \${rootpart} console=\${console},\${baudrate} \${smp} flux=fluxdata\${labelpre}"
- fi
-else
+setenv bootargs \${fdtargs} \${bootpart} ostree=/ostree/\${ostver} \${rootpart} console=\${console},\${baudrate} \${smp} flux=fluxdata\${labelpre}
+if test ! -n \${use_fdtdtb} || test \${use_fdtdtb} -lt 1; then
  setenv loaddtb ext4load mmc \${mmcdev}:\${mmcpart} \${fdt_addr} \${ostver}/\${fdt_file};run loaddtb
 fi
 run loadramdisk

commit f02351f28635542c51912aa9f07812a9b0405342

boot.scr: Add missing quotes for bootargs
The bootargs require quotes to deal with arguments in the dtb data.
This is a regression from commit: bb868b27706ca926

[ Issue: LINCD-703 ]

Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

recipes-bsp/u-boot/u-boot-uenv_2.0.bb
@@ -203,7 +203,7 @@ fi
 if test \${skip_script_wd} != yes; then setenv wdttimeout 120000; fi
 setenv loadkernel ext4load mmc \${mmcdev}:\${mmcpart} \${loadaddr} \${ostver}/vmlinuz
 setenv loadramdisk ext4load mmc \${mmcdev}:\${mmcpart} \${initrd_addr} \${ostver}/initramfs
-setenv bootargs \${fdtargs} \${bootpart} ostree=/ostree/\${ostver} \${rootpart} console=\${console},\${baudrate} \${smp} flux=fluxdata\${labelpre}
+setenv bootargs "\${fdtargs} \${bootpart} ostree=/ostree/\${ostver} \${rootpart} console=\${console},\${baudrate} \${smp} flux=fluxdata\${labelpre}"
 if test ! -n \${use_fdtdtb} || test \${use_fdtdtb} -lt 1; then
  setenv loaddtb ext4load mmc \${mmcdev}:\${mmcpart} \${fdt_addr} \${ostver}/\${fdt_file};run loaddtb
 fi