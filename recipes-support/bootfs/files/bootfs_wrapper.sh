4a8c1483e9db3902eb7eb6eab91b64fbbc44800c
Hongxu Jia <hongxu.jia@windriver.com>
2020-08-05 15:56:26 +0800

    nativesdk-bootfs: bootfs.sh in sdk must use option -e <envfile> mandatory
    The bootfs.sh in sdk does not have yocto build (no bitbake -e to
    generate env file), then it must use option -e <envfile> mandatory
    
    Only pick up DEPLOY_DIR_IMAGE from <envfile> in wrapper, others will
    be parsed in real bootfs.sh
    
    Issue: LINUXEXEC-7219
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- recipes-support/bootfs/files/bootfs_wrapper.sh
+++ recipes-support/bootfs/files/bootfs_wrapper.sh
@@ -15,19 +15,31 @@
 #* You should have received a copy of the GNU General Public License
 #* along with this program; if not, write to the Free Software
 #* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ENVFILE="none"
+
 while getopts "a:Bb:d:e:hLl:Nns:u:w" opt; do
     case ${opt} in
         e)
             ENVFILE=$OPTARG
             ;;
         h)
-            $OECORE_NATIVE_SYSROOT/usr/share/bootfs/scripts/bootfs.sh --help
+            $OECORE_NATIVE_SYSROOT/usr/share/bootfs/scripts/bootfs.sh -h
             exit 0
     esac
 done
 
+if [ "$ENVFILE" = "none" ]; then
+    echo "Error: Mandatory using option -e <envfile>"
+    exit 1
+fi
+
+if [ ! -e $ENVFILE ] ; then
+    echo "Error: The \`$ENVFILE' does not exist"
+    exit 1
+fi
+
 export IMAGE_ROOTFS="$PWD/bootfs"
 export DEPLOY_DIR_IMAGE
-eval `cat $ENVFILE`
+eval `grep ^DEPLOY_DIR_IMAGE $ENVFILE`
 
 $OECORE_NATIVE_SYSROOT/usr/share/bootfs/scripts/bootfs.sh $@
