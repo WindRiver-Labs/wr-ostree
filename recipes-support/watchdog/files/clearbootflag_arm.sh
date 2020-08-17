66018e3ba4fa3e06137b2b4796ca884f822392b3
Jason Wessel <jason.wessel@windriver.com>
2020-08-17 09:52:00 -0700

    watchdog: Do not depend on blkid
    Use the first partition fixes it to use the first partition on the
    same volume as the sysroot.  In case where an sdcard and usb device
    are inserted in the system with the same disk labels sometimes the
    wrong volume is mounted.
    
    [ Issue: LINCD-2833 ]
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- recipes-support/watchdog/files/clearbootflag_arm.sh
+++ recipes-support/watchdog/files/clearbootflag_arm.sh
@@ -15,7 +15,9 @@
 #* along with this program; if not, write to the Free Software
 #* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-part=`blkid --label boot`
+partbase=`cat /proc/mounts |grep "sysroot " | awk '{print $1}'`
+### ASSUME sysroot is on a partitio <= 9 ###
+part=${partbase::-1}'1'
 tdir=`mktemp -d`
 if [ "$tdir" != "" ] ; then
 	mount ${part} ${tdir}
