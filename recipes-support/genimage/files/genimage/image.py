20ee8c3b87050813436a85631e3284a96f8c86e3
Hongxu Jia <hongxu.jia@windriver.com>
2020-10-15 22:35:45 +0800

    genimage: disable pseudo for the generation of ustart image
    The calling of bootfs.sh does not require pseudo
    
    Issue: LINUXEXEC-10241
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- recipes-support/genimage/files/genimage/image.py
+++ recipes-support/genimage/files/genimage/image.py
@@ -315,9 +315,11 @@ class CreateBootfs(Image):
     def create(self):
         self._write_readme("ustart")
 
+        ustart_env = os.environ.copy()
+        del ustart_env['LD_PRELOAD']
         cmd = os.path.expandvars("$OECORE_NATIVE_SYSROOT/usr/bin/bootfs.sh")
         cmd = "{0} -L -a instdate=BUILD_DATE -s 0 -e {1}/{2}-{3}.env".format(cmd, self.deploydir, self.image_name, self.machine)
-        res, output = utils.run_cmd(cmd, shell=True, cwd=self.workdir)
+        res, output = utils.run_cmd(cmd, shell=True, cwd=self.workdir, env=ustart_env)
         if res:
             raise Exception("Executing %s failed\nExit code %d. Output:\n%s"
                                % (cmd, res, output))
