4b4ce1043dfaa26b7c36641c0b92fdc2c0287407
Hongxu Jia <hongxu.jia@windriver.com>
2020-11-20 17:48:51 +0800

    genimage: implement new lived containers mechanism
    Improve update_containers.sh:
    - Support to load docker image by docker load
    - Support to import filesystem tarball by docker import
    - Support to pull docker image from registry
    - Support run and start multiple containers from one image
    - Support to customize docker run option
    - Support to customize docker run command
    - If any container in containers.dat is updated failed, do not touch
      the stamp of update_containers.sh which makes the script be rerun
      at next boot
    
    Improve start_containers.sh:
    - Start container by systemd start service rather than docker start
      which makes the container be stopped gracefully while reboot
    
    Each line in containers.dat records how to load/import/pull and run a container
    The format of line is:
    The `<container-name>' is mandatory, it is the name of container (docker run --name <container-name> XXX);
    If `load=<docker-image-tarball>' is set, use `docker load' to add image tarball;
    If `import=<fs-tarball>' is set, use `docker import' to add filesystem tarball;
    If no `load=<docker-image-tarball>' and no `import=<fs-tarball>', use `docker pull' to add image;
    The `image=<container-image-name>' is optional, if not set, use `<container-name>' by default;
    The `run-opt=<docker-run-opt>' is optional, if not set, use `-itd' by default (docker run -itd XXX);
    The `run-cmd=<docker-run-cmd>' is optional, if not set, default is empty
    Examples:
      ubuntu
        ->        docker pull ubuntu
        ->        docker run --name ubuntu ubuntu
        ->        systemctl start start-container@ubuntu.service
    
      ubuntu-bash image=ubuntu run-opt="-p 2224:22 -it" run-cmd=/bin/bash
        ->        docker pull ubuntu
        ->        docker run -p 2224:22 -it -d --name ubuntu-bash ubuntu /bin/bash
        ->        systemctl start start-container@ubuntu-bash.service
    
      wrlinux-image-full import=/var/docker-images/wrlinux-image-full-intel-x86-64.tar.bz2 run-cmd=/bin/sh
        ->        docker import /var/docker-images/wrlinux-image-full-intel-x86-64.tar.bz2 wrlinux-image-full
        ->        docker run -itd --name wrlinux-image-full wrlinux-image-full /bin/sh
        ->        systemctl start start-container@wrlinux-image-full.service
    
      container-base load=/var/docker-images/container-base-intel-x86-64.docker-image.tar.bz2
        ->        docker load -i /var/docker-images/container-base-intel-x86-64.docker-image.tar.bz2
        ->        docker run -itd --name container-base
        ->        systemctl start start-container@container-base.service
    
    On target:
    root@intel-x86-64:~# docker ps
    CONTAINER ID        IMAGE                         COMMAND             CREATED              STATUS              PORTS                  NAMES
    a285c788bf71        ubuntu                        "/bin/bash"         About a minute ago   Up About a minute   0.0.0.0:2224->22/tcp   ubuntu-bash
    c4453e2bba7d        ubuntu                        "/bin/bash"         About a minute ago   Up About a minute                          ubuntu
    1429b1b995ee        wrlinux-image-full            "/bin/sh"           2 minutes ago        Up 2 minutes                               wrlinux-image-full
    38ebdcef9557        container-base-intel-x86-64   "/bin/sh"           2 minutes ago        Up 2 minutes                               container-base
    
    Issue: LINUXEXEC-7235
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- recipes-support/genimage/files/genimage/data/yaml_example/sysdef/run_always.d/start_containers.sh
+++ recipes-support/genimage/files/genimage/data/yaml_example/sysdef/run_always.d/start_containers.sh
@@ -2,7 +2,11 @@
 # Start containers listed in /etc/sysdef/run_on_upgrade.d/XXXX/containers.dat
 latest=`ls /etc/sysdef/run_on_upgrade.d/ -1 -v -r | head -n 1`
 dirname="/etc/sysdef/run_on_upgrade.d/${latest}"
-dat=`cat ${dirname}/containers.dat`
-for container_name in ${dat}; do
-    docker start ${container_name}
-done
+dat="${dirname}/containers.dat"
+while read -r line; do
+    [ "${line}" != "${line#\#}" ] && continue
+    container_name=${line%% *}
+    [ -z "${container_name}" ] && continue
+    echo "systemctl start start-container@${container_name}.service"
+    systemctl start start-container@${container_name}.service
+done < ${dat}
