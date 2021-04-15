ca64933438749ef964d05210dbf68c7138d3920f
Chen Qi <Qi.Chen@windriver.com>
2021-04-14 22:11:35 -0700

    LAT: add copyright and GPL-2.0 license notice
    Issue: LINUXEXEC-13278
    
    Add copyright and license notice to LAT files.
    
    The files included:
    1) bb files
    2) python script files
    3) shell script files
    
    Files excluded for now:
    1) configuration files, e.g. yaml file
    2) .inc files, which are included by bb files
    3) .patch files
    4) README files
    5) run.xxx shell script which are basically from yocto
    
    Signed-off-by: Chen Qi <Qi.Chen@windriver.com>

--- recipes-support/genimage/files/genimage/data/yaml_example/sysdef/run_always.d/10_start_containers.sh
+++ recipes-support/genimage/files/genimage/data/yaml_example/sysdef/run_always.d/10_start_containers.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 # Start containers listed in /etc/sysdef/run_on_upgrade.d/XXXX/containers.dat
+#
+# Copyright (c) 2021 Wind River Systems, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
 latest=`ls /etc/sysdef/run_on_upgrade.d/ -1 -v -r | head -n 1`
 dirname="/etc/sysdef/run_on_upgrade.d/${latest}"
 dat="${dirname}/containers.dat"
--- recipes-support/genimage/files/genimage/data/yaml_example/sysdef/run_on_upgrade.d/10_update_containers.sh
+++ recipes-support/genimage/files/genimage/data/yaml_example/sysdef/run_on_upgrade.d/10_update_containers.sh
@@ -11,6 +11,22 @@
 # The `run-opt=<docker-run-opt>' is optional, if not set, use `-itd' by default (docker run -itd XXX);
 # The `run-cmd=<docker-run-cmd>' is optional, if not set, default is empty
 # If an old container has already existed, rename it
+#
+# Copyright (c) 2021 Wind River Systems, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
 KEYS="load import image run-opt run-cmd"
 get_val() {
     local key=$1
